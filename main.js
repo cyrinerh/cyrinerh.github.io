d90b77c1bfc7a9aafad7f5d5b4f43990aa191c28
cyrine <cyrine.rihane@gmail.com>
2017-11-19 20:53:07 +0100

    scroll code changed

--- main.js
+++ main.js
@@ -1,52 +1,14 @@
 var navLinks = document.querySelectorAll('nav a')
-var firstLink = navLinks[1]
 
-function linkToHash(link) {
-  return link.hash
-}
-
-firstLink.onclick = function(event) {
-  event.preventDefault()
-  var targetDiv = document.querySelector('#about')
-  $('html, body').animate(
-  {
-    scrollTop: targetDiv.offsetTop
-  },
-  1000
-  )
-}
-
-var secondLink = navLinks[2]
-
-function linkToHash(link) {
-  return link.hash
-}
-
-secondLink.onclick = function(event) {
-  event.preventDefault()
-  var targetDiv = document.querySelector('#portfolio')
-  $('html, body').animate(
-  {
-    scrollTop: targetDiv.offsetTop
-  },
-  1000
-  )
-}
-
-
-var thirdLink = navLinks[3]
-
-function linkToHash(link) {
-  return link.hash
-}
-
-thirdLink.onclick = function(event) {
-  event.preventDefault()
-  var targetDiv = document.querySelector('#contact')
-  $('html, body').animate(
-  {
-    scrollTop: targetDiv.offsetTop
-  },
-  2000
-  )
-}
+navLinks.forEach(function(link){
+  link.onclick = function(event) {
+    event.preventDefault()
+    var targetDiv = document.querySelector(event.target.hash)
+    $('html, body').animate(
+    {
+      scrollTop: targetDiv.offsetTop
+    },
+    1000
+    )
+  }
+})
